0e36fb358192ed6596a65fd07610f3f07321bc40
Arpit- Kahale <arpitkahalek13@gmail.com>
2025-10-23 16:08:34 +0530

    pushed the new file

---
+++ comparision.js
@@ -0,0 +1,25 @@
+let price=100;
+let discount="100";
+
+// equal its only going to only cheack ,booolean value
+
+console.log(price==discount);//true
+
+// strict equal or equality .. its going to cheack both value and the data type 
+console.log(price===discount)// false cheack both datatype and value
+
+
+// not equal to
+
+console.log(price!==discount)//true 
+
+//  strict not equal to 
+
+console.log(price!==50)//true
+
+// less than equal to 
+console.log(price>=100)
+
+console.log(price>=discount)
+
+console.log(price<=50)
