8f2adefb5a4f00e7fbf818bf1a0e0bf37251b774
Arpit- Kahale <arpitkahalek13@gmail.com>
2025-10-23 19:49:21 +0530

    pushed the new file

---
+++ logical.js
@@ -0,0 +1,29 @@
+// LOGICAL OPERTATOR
+
+let a= true;
+let b= false;
+// let us dicuss loical and first &&
+
+console.log(true && true);// true
+console.log(true && false);// false
+console.log(false && true);// false
+console.log(false && false);// false
+
+// logical or || operetor
+console.log(true || true);// true
+console.log(true || false);// true
+console.log(false|| true);// true
+console.log(false || false);// false
+  
+// and 
+
+console.log(a&&b)
+
+// or 
+
+console.log(a||b)
+
+
+//  not 
+console.log(!a)
+console.log(!b)
