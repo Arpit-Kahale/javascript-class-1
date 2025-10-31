1adb736b4c82bba5935a43ffaffc1b555b78e277
Arpit- Kahale <arpitkahalek13@gmail.com>
2025-10-31 23:26:23 +0530

    pushed all changes

---
+++ ifelse.js
@@ -0,0 +1,41 @@
+// If and else condition
+
+
+let age =18
+if (age>=15){
+    console.log("you are adult")
+}
+else{
+    console.log("you are minor")
+}
+
+
+
+
+let marks=75;
+
+if(marks>=90){
+    console.log("Grade:A")
+}
+else if(marks>=70){
+    console.log("Grade:B")
+}else if(marks>=50){
+    console.log("Grade:C")
+}else{
+    console.log("FAIL")
+}
+
+///Login to the  website  
+
+let username="venkat";
+let password="venkat@1234";
+
+// cheack login 
+
+if(username==="venkat" && password==="venkat@1234")
+    {
+    console.log("Login susessfull")
+}
+else{
+    console.log("Invalid username and password")
+}
---
+++ switch.js
@@ -0,0 +1,33 @@
+// switch case ..... when wanted to compare the single value with multiple cases 
+// then we use the switch case...
+
+
+let signal="yellow";
+
+
+switch(signal){
+
+    case"red":
+    console.log("goooooo");
+    break;
+
+    case"blue":
+    console.log("go and slow");
+    break;
+
+    case "pink":
+    console.log("go fastttt");
+    break;
+
+    case"yellow":
+    console.log("go slow and noraml")
+    break;
+  
+
+    case"blue":
+    console.log("stop here")
+    break;
+
+}
+
+
